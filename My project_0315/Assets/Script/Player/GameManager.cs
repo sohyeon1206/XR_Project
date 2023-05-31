e71439b4d3a30d5032e494c16649d65195dff921
Lee Soheyon <lee21083843@gmail.com>
2023-05-31 21:02:30 +0900

    빌드 테스트까지 진행

--- My project_0315/Assets/Script/Player/GameManager.cs
+++ My project_0315/Assets/Script/Player/GameManager.cs
@@ -1,24 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;       // UI 추가
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;               // UI 추가 
+using System;                       // 추가 
 
 public class GameManager : MonoBehaviour
 {
-    public GameManager() { }
-    public static GameManager Instance { get; private set; }    // 싱글톤화
-
-    public PlayerHp playerHp;               // 플레이어의 HP
-    public Image playerHpUIImage;           // 플레이어 Hp UI 이미지
-    public Button BtnSample;
-
-    private void Start()
+    protected SceneChanger SceneChanger => SceneChanger.Instance;
+    public enum GameState           //게임 상태값 설정
     {
-        this.BtnSample.onClick.AddListener(() => 
-        {
-            Debug.Log("Button Check");
-        });
+        Start,
+        Playing,
+        GameOver
     }
+
+    public event Action<GameState> OnGameStateChanged;
+
+    public GameState currentState = GameState.Start;
+
+    public GameState CurrentState
+    {
+        get { return currentState; }
+        private set
+        {
+            currentState = value;
+            OnGameStateChanged?.Invoke(currentState);       //이벤트가 null이 아닌경우에만 이 이벤트를 호출 
+        }
+    }
+
+    public void StartGame()
+    {   //게임 시작 로직을 여기에 작성
+        CurrentState = GameState.Playing;
+    }
+
+    public void GameOver()
+    {   //게임 오버 로직을 여기에 작성
+        CurrentState = GameState.GameOver;
+        SceneChanger.LoadEndScene();
+    }
+
+    public GameManager() { }
+    public static GameManager Instance { get; private set; }    //싱글톤화
+    public PlayerHp playerHp;                   //플레이어의 Hp
+    public Image playerHpUIImage;               //플레이어 Hp UI 이미지
+    public Button BtnSample;                    //UI 버튼 선언
 
     private void Awake()
     {
@@ -33,17 +59,32 @@ public class GameManager : MonoBehaviour
             Instance = this;
             DontDestroyOnLoad(gameObject);
         }
+        SceneManager.sceneLoaded += OnSceneLoaded;
         Init();
+    }
+
+    private void OnDestroy()    //이 오브젝트가 파괴될 경우
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;  //이벤트를 삭제한다. 
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if(scene.name == "GameScene")
+        {
+            Init();
+        }
     }
 
     private void Init()
     {
-        playerHp = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHp>();                     // Tag로 오브젝트를 찾는다.
-        playerHpUIImage = GameObject.FindGameObjectWithTag("UIHealthBar").GetComponent<Image>();            // Tag로 UI를 찾는다.
+        playerHp = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHp>();                     //Tag로 오브젝트를 찾는다.
+        playerHpUIImage = GameObject.FindGameObjectWithTag("UIHealthBar").GetComponent<Image>();            //Tag로 UI를 찾는다.
+        playerHp.Hp = 100;
+        CurrentState = GameState.Start;
     }
-
     private void Update()
     {
-        playerHpUIImage.fillAmount = (float)playerHp.Hp / 100.0f;                                           // 체력에 비례하게 작업
+        playerHpUIImage.fillAmount = (float)playerHp.Hp / 100.0f;                                            //체력에 비례하게 작업
     }
 }
